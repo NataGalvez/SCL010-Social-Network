141e468bbf86542f39adf0408efa21fac75051d7
NataGalvez <nataliac.galvezc@gmail.com>
2019-08-07 14:47:41 -0400

    se elimina archivo firebaseInit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 console.log("holi");
 
 import { initRouter } from "./route.js";
-import {firebaseInit} from "./assets/js/firebaseInit.js";
-
 const init = () => {
-	firebaseInit();
     initRouter();
 }
 
