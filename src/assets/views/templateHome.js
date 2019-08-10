5a44b1c4eb294390f9fe2dde3ff422192075d1de
Scarlette Villarreal Á <villarrealscarlette@gmail.com>
2019-08-10 17:55:04 -0400

    changes in css

--- src/assets/views/templateHome.js
+++ src/assets/views/templateHome.js
@@ -1,3 +1,10 @@
-// export const templateHome = () => {
-//     const cointainerHome = document.getElementById("main");
-//     const contentHome = 
+export const templateHome = () => {
+    const cointainerHome = document.createElement("div");
+    cointainerHome.className = "container-img"
+    const contentHome = `
+<div class="container-img">
+<span type="image" class="template" alt="Cancha de fútbol"><h4>¡Ingresa - Confirma - Juega!</h4></span>
+</div>`
+    cointainerHome.innerHTML = contentHome;
+    return cointainerHome;
+} 
--- src/assets/views/templateLogin.js
+++ src/assets/views/templateLogin.js
@@ -1,25 +1,25 @@
 import { loginAccount } from "../js/auth.js";
 import { loginGoogle } from "../js/auth.js";
-import { loginFacebook } from "./../js/auth.js"
+import { loginFacebook } from "./../js/auth.js";
 //Contenedor y contenido que se va a mostrar al aparecer el Template
 export const templateLogin = () => {
     const containerLogin = document.createElement("div");
     const contentLogin = `
 <div class="container login">
     <div class="login-content">
+        <button id="loginGoogle" class="login-google btn">Iniciar sesión con Google</button>
+        <button id="loginFacebook" class="login-facebook btn">Iniciar sesión con Facebook</button>
         <input type="email" id="emailLogin" placeholder="Correo Electrónico">
-        <input type="password" id="passwordLogin" placeholder="Contraseña"><a href="#">¿Se te olvidó tu contraseña?</a>
-        <button id="login" class="login-btn-account">Iniciar sesión</button>
-        <button id="loginGoogle" class="login-btn-google">Iniciar sesión con Google</button>
-        <button id="facebook" class="btn">Iniciar sesión con Facebook</button>
+        <input type="password" id="passwordLogin" placeholder="Contraseña"><p><a href="#">¿Se te olvidó tu contraseña?</a></p>
+        <button id="login" class="login btn">Iniciar sesión</button>
         <br>
-        <p>¿No tienes una cuenta?<a href="#create" class="createAccount">Regístrate</a></p>
+        <p>¿No tienes una cuenta?<a href="#/create" class="createAccount">Regístrate</a></p>
     </div>
 </div>`
 //Imprimir contenido en el container
 containerLogin.innerHTML = contentLogin;
 //Boton de acceder con facebook
-const btnFacebook = containerLogin.querySelector("#facebook");
+const btnFacebook = containerLogin.querySelector("#loginFacebook");
 btnFacebook.addEventListener("click", () => {
     loginFacebook();
 })
