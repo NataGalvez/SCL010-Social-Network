5f3ae7a19f5854a5371150cbf2296ce82d85e42c
NataGalvez <nataliac.galvezc@gmail.com>
2019-08-19 14:33:47 -0400

    Comit Profile

--- src/assets/views/templateProfile.js
+++ src/assets/views/templateProfile.js
@@ -1,5 +1,13 @@
-import {templateAditionalInfo} from "./templateAditionalInfo.js"
+import {templateAditionalInfo} from "./templateAditionalInfo.js";
+import { loginGoogle} from "../js/auth.js";
 export const templateProfile = () =>{
-document.getElementById("root").innerHTML = window.currenUser + window.positionSelect + window.regionSelect;
+const containerProfile = document.createElement("div");
+containerProfile.clasName = "profile";
+let contentProfile = firebase.firestore().collection("Users").doc(result.user.uid).get().then((userData)=>{
+     let user = userData.data();
+     user.type;
+     user.region;
+     containerAditionalInfo.innerHTML = contentInfo;
+    return templateProfile; 
+})
 }
-templateProfile();
